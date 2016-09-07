88e7209d35d47fd244a68a6befc1949eafca60a2
trikitrok <trikitrok@gmail.com>
2016-09-07 22:47:24 +0200

    formatting code

--- test/cellular_automata/core_test.clj
+++ test/cellular_automata/core_test.clj
@@ -1,26 +1,26 @@
 (ns cellular-automata.core-test
-  (:require [midje.sweet :refer :all]
-            [cellular-automata.core :as automata]
-            [cellular-automata.rules :as rules]))
+  (:require
+    [midje.sweet :refer :all]
+    [cellular-automata.core :as automata]
+    [cellular-automata.rules :as rules]))
 
 (facts
   "about cellular automata"
 
   (facts
     "about evolving an automaton"
-
     (automata/evolve
       rules/rule-90
-      [1 1 0 1 0 1 0] 5) => [[1 1 0 1 0 1 0]
-                             [1 1 0 0 0 0 1]
-                             [1 1 1 0 0 1 0]
-                             [1 0 1 1 1 0 1]
-                             [0 0 1 0 1 0 0]
-                             [0 1 0 0 0 1 0]])
+      [1 1 0 1 0 1 0]
+      5) => [[1 1 0 1 0 1 0]
+             [1 1 0 0 0 0 1]
+             [1 1 1 0 0 1 0]
+             [1 0 1 1 1 0 1]
+             [0 0 1 0 1 0 0]
+             [0 1 0 0 0 1 0]])
 
   (facts
     "about rendering an automaton"
-
     (automata/render
       [[1 1 0 1 0 1 0]
        [1 1 0 0 0 0 1]
